650129fc052e638e3d090671e6c6caec35302b2a
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-06-04 11:13:37 +0200

    Test GPU vs CPU logdetM

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <cmath>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <vector>
 
 #include <blaze/Blaze.h>
 
 #include "cpu/cpu.hpp"
+#include "gpu/gpu.cuh"
 
 auto makeTriangle()
 {
@@ -19,17 +21,39 @@ auto makeTriangle()
     return hopping;
 }
 
+template <typename T>
+auto flatMatrix(blaze::DynamicMatrix<T, blaze::columnMajor> const &matrix)
+{
+    std::vector<T> flat(matrix.rows() * matrix.columns());
+    for (std::size_t i = 0; i < matrix.rows(); ++i) {
+        for (std::size_t j = 0; j < matrix.columns(); ++j) {
+            flat[j * matrix.rows() + i] = matrix(i, j);
+        }
+    }
+    return flat;
+}
+
 
 int main()
 {
     auto hopping = makeTriangle();
-    constexpr size_t nt = 32;
+    constexpr size_t nt = 64;
     constexpr double U = 4;
     constexpr double beta = 6;
 
-    blaze::DynamicVector<std::complex<double>> phi(hopping.rows()*nt, 0.0);
+    blaze::DynamicVector<std::complex<double>> phi(hopping.rows() * nt, 1.0);
+    std::mt19937 rng(12456);
+    std::normal_distribution<double> dist(0, sqrt(U * beta / nt));
+    std::generate(phi.begin(), phi.end(), [&]() mutable { return dist(rng); });
 
     auto cpuLDM = cpu::logdetM(hopping, phi, beta);
     std::cout << "CPU: " << cpuLDM[0] << ' ' << cpuLDM[1] << '\n';
 
+
+    auto gpuLDM = gpu::logdetM(flatMatrix(blaze::DynamicMatrix<double, blaze::columnMajor>(hopping * beta / nt)).data(),
+                               phi.data(),
+                               hopping.rows(),
+                               nt,
+                               beta);
+    std::cout << "GPU: " << gpuLDM[0] << ' ' << gpuLDM[1] << '\n';
 }
